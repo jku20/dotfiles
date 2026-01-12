2af40509286b3f5d9b0f22a8743feac6bd1d3c5b
jeremy <jeremykubenjetschool@gmail.com>
2026-01-12 18:05:13 -0500

    Move aliases into config.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,11 @@
 set -g fish_greeting
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+    # distrobox aliases
+    alias dbuild="distrobox assemble create --file"
+    function drebuild
+        distrobox rm $argv && dbuild $argv
+    end
+
+    # dotfiles alias
+    alias dotfiles='git --git-dir="$HOME/.dotfiles/" --work-tree="$HOME"'
 end
